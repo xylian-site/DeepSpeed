0ecd5b85a2e0fe3379a8195777d3cdba2bcc9d61
Masahiro Tanaka <mtanaka@microsoft.com>
2024-09-11 21:08:26 +0000

    fix sync of profiler

--- deepspeed/runtime/zero/compile/profile.py
+++ deepspeed/runtime/zero/compile/profile.py
@@ -48,39 +48,37 @@ class ProfilingInterpreter(Interpreter):
                 return t
 
             with maybe_disable_fake_tensor_mode():
-                args, kwargs = self.fetch_args_kwargs_from_env(n)
-                assert isinstance(args, tuple)
-                assert isinstance(kwargs, dict)
+                with get_accelerator().random().fork_rng(devices=[get_accelerator().current_device_name()]):
+                    args, kwargs = self.fetch_args_kwargs_from_env(n)
+                    assert isinstance(args, tuple)
+                    assert isinstance(kwargs, dict)
 
-                args = tree_map(to_device, args)
-                kwargs = tree_map(to_device, kwargs)
-                walltimes = []
+                    args = tree_map(to_device, args)
+                    kwargs = tree_map(to_device, kwargs)
+                    walltimes = []
 
-                if is_comm_op(n):
-                    dist.barrier()
+                    if is_comm_op(n):
+                        dist.barrier()
 
-                for i in range(self.iteration):
-                    start = time.time()
-                    start_events[i].record()
-                    getattr(self, n.op)(n.target, args, kwargs)
-                    end_events[i].record()
-                    walltimes.append(time.time() - start)
+                    for i in range(self.iteration):
+                        start = time.time()
+                        start_events[i].record()
+                        getattr(self, n.op)(n.target, args, kwargs)
+                        end_events[i].record()
+                        walltimes.append(time.time() - start)
 
-                if is_comm_op(n):
-                    dist.barrier()
+                    if is_comm_op(n):
+                        dist.barrier()
 
-                accelerator.synchronize()
+                    accelerator.synchronize()
 
             device_time = statistics.mean([s.elapsed_time(e) for s, e in zip(start_events, end_events)][self.warmup:])
             wall_time = statistics.mean(walltimes[self.warmup:]) * 1000
+
             with maybe_disable_fake_tensor_mode():
                 vals_to_bcast = torch.tensor([device_time, wall_time], device=device)
                 dist.broadcast(vals_to_bcast, 0)
                 n.meta["device_time"] = vals_to_bcast[0].item()
                 n.meta["wall_time"] = vals_to_bcast[1].item()
-
-            n.meta["device_time"] = statistics.mean([s.elapsed_time(e)
-                                                     for s, e in zip(start_events, end_events)][self.warmup:])
-            n.meta["wall_time"] = statistics.mean(walltimes[self.warmup:]) * 1000
 
         return fake_ret

aadac9daedd6677620a9f8b8d2a6de56a3ca112f
Masahiro Tanaka <mtanaka@microsoft.com>
2024-09-11 01:24:42 +0000

    sync profiling results

--- deepspeed/runtime/zero/compile/profile.py
+++ deepspeed/runtime/zero/compile/profile.py
@@ -10,8 +10,11 @@ import statistics
 import torch
 from torch.utils._pytree import tree_map
 from torch.fx import GraphModule, Interpreter
+from torch.fx.experimental.proxy_tensor import maybe_disable_fake_tensor_mode
 
+import deepspeed.comm as dist
 from deepspeed.accelerator import get_accelerator
+from .util import is_comm_op
 
 
 # https://pytorch.org/tutorials/intermediate/fx_profiling_tutorial.html
@@ -44,28 +47,38 @@ class ProfilingInterpreter(Interpreter):
                     return t.to(device)
                 return t
 
-            args, kwargs = self.fetch_args_kwargs_from_env(n)
-            assert isinstance(args, tuple)
-            assert isinstance(kwargs, dict)
+            with maybe_disable_fake_tensor_mode():
+                args, kwargs = self.fetch_args_kwargs_from_env(n)
+                assert isinstance(args, tuple)
+                assert isinstance(kwargs, dict)
 
-            args = tree_map(to_device, args)
-            kwargs = tree_map(to_device, kwargs)
-            walltimes = []
+                args = tree_map(to_device, args)
+                kwargs = tree_map(to_device, kwargs)
+                walltimes = []
 
-            # if is_comm_op(n):
-            #     dist.barrier()
+                if is_comm_op(n):
+                    dist.barrier()
 
-            for i in range(self.iteration):
-                start = time.time()
-                start_events[i].record()
-                getattr(self, n.op)(n.target, args, kwargs)
-                end_events[i].record()
-                walltimes.append(time.time() - start)
+                for i in range(self.iteration):
+                    start = time.time()
+                    start_events[i].record()
+                    getattr(self, n.op)(n.target, args, kwargs)
+                    end_events[i].record()
+                    walltimes.append(time.time() - start)
 
-            # if is_comm_op(n):
-            #     dist.barrier()
+                if is_comm_op(n):
+                    dist.barrier()
 
-            accelerator.synchronize()
+                accelerator.synchronize()
+
+            device_time = statistics.mean([s.elapsed_time(e) for s, e in zip(start_events, end_events)][self.warmup:])
+            wall_time = statistics.mean(walltimes[self.warmup:]) * 1000
+            with maybe_disable_fake_tensor_mode():
+                vals_to_bcast = torch.tensor([device_time, wall_time], device=device)
+                dist.broadcast(vals_to_bcast, 0)
+                n.meta["device_time"] = vals_to_bcast[0].item()
+                n.meta["wall_time"] = vals_to_bcast[1].item()
+
             n.meta["device_time"] = statistics.mean([s.elapsed_time(e)
                                                      for s, e in zip(start_events, end_events)][self.warmup:])
             n.meta["wall_time"] = statistics.mean(walltimes[self.warmup:]) * 1000

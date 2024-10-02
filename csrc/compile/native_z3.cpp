d7c9ef5ec8681ac8e5b44fd37531bec0bfe4ef58
Masahiro Tanaka <mtanaka@microsoft.com>
2024-10-02 01:53:35 +0000

    use non-blocking copy for symm memory

--- csrc/compile/native_z3.cpp
+++ csrc/compile/native_z3.cpp
@@ -390,7 +390,7 @@ public:
 
             at::Tensor local_buf =
                 symm_mem->get_buffer(rank, ds_tensor.sizes(), ds_tensor.scalar_type(), 0);
-            local_buf.copy_(ds_tensor);
+            local_buf.copy_(ds_tensor, true);
 
             symm_mem->barrier(0);
             auto chunks = output_buf.flatten().chunk(world_size);
@@ -398,7 +398,7 @@ public:
                 int remote_rank = (rank - step + world_size) % world_size;
                 auto src_buf = symm_mem->get_buffer(
                     remote_rank, ds_tensor.sizes(), ds_tensor.scalar_type(), 0);
-                chunks[remote_rank].copy_(src_buf.flatten());
+                chunks[remote_rank].copy_(src_buf.flatten(), true);
             }
             symm_mem->barrier(0);
         }
